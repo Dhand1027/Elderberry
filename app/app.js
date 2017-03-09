3637afebd8f4177787890c739fa892370450f655
Eric Kwan <ekwan@umass.edu>
2017-03-09 16:37:39 -0500

    start up 3 scaffolding

--- app/app.js
+++ app/app.js
@@ -1,1 +1,40 @@
 /* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Each major browser view user interface must be imported.
+import UIcalender from './components/ui-calender.js';
+import UIfileEditor from './components/ui-fileEditor.js';
+import UIfileExplorer from './components/ui-fileExplorer.js';
+import UImaps from './components/ui-maps.js';
+import UInotes from './components/ui-notes.js';
+
+
+// For each view conditionally determine which view to display
+// depending on if the ID is present in the HTML.
+if (document.getElementById('ui-calender') !== null) {
+  ReactDOM.render(
+    <UIcalender />,
+    document.getElementById('ui-calender')
+  );
+} else if (document.getElementById('ui-fileEditor') !== null) {
+  ReactDOM.render(
+    <UIfileEditor />,
+    document.getElementById('ui-fileEditor')
+  );
+} else if (document.getElementById('ui-fileExplorer') !== null) {
+  ReactDOM.render(
+    <UIfileExplorer />,
+    document.getElementById('ui-fileExplorer')
+  );
+} else if (document.getElementById('ui-maps') !== null) {
+  ReactDOM.render(
+    <UImaps />,
+    document.getElementById('ui-maps')
+  );
+} else if (document.getElementById('ui-notes') !== null) {
+  ReactDOM.render(
+    <UInotes />,
+    document.getElementById('ui-notes')
+  );
+}
