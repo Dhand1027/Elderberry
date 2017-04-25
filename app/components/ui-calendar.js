fbc319d15ab3244824537627f996fc6159689ad2
Dylan Hand <dhand@umass.edu>
2017-04-25 16:29:56 -0400

    Pushing template just so it's saved

--- app/components/ui-calendar.js
+++ app/components/ui-calendar.js
@@ -5,10 +5,30 @@ export default class UIcalendar extends React.Component {
   render() {
     return (
 
-      <div className="container-fluid">
-        
-      </div>
+      <div classNameName="container-fluid">
+          <div className="row" >
+            <div className="col-md-10 col-xs-9 background">
+              <div className="text-center" >
+                <div className="container">
+                  <div className="row row-height">
+                    <div className="col-md-12 boxing">
+                      <div className="tabbable-panel-maps">
+                        <div className="tabbable-line">
 
+                          <h2>Calendar</h2>
+
+                               (Calendar here)
+
+
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+      </div>
     )
   }
 }
