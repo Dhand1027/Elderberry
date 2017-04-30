b6635c34b58309a7beb2aa0ebb4df3155d2d2ff9
Antonio Borges <apborges@umass.edu>
2017-04-30 19:27:35 -0400

    complete makeover, added search bar

--- app/components/ui-fileExplorer.js
+++ app/components/ui-fileExplorer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-
-import Files from './files.js'
+import Files from './files.js';
 import {getData} from '../server.js';
 
 export default class UIfileExplorer extends React.Component {
@@ -35,13 +34,65 @@ export default class UIfileExplorer extends React.Component {
                         <h2>Files</h2>
                         <div className="contentBox">
 
-                          <div className= "col-md-12">
-                            {this.state.contents.map((Data) => {
-                              return(
-                                <Files key={Data.idx} data={Data} />
-                              )
-                            })}
-                          </div>
+                          <div className="col-md-10 col-md-offset-1">
+             <form action="#" method="get">
+                 <div className="input-group btn-padding">
+                     <input className="form-control" id="system-search" name="q" placeholder="Search for" required></input>
+                     <span className="input-group-btn">
+                         <button type="submit" className="btn btn-default btn-md" ><i className="glyphicon glyphicon-search"></i></button>
+                     </span>
+                 </div>
+             </form>
+                    </div>
+
+<div className="well-sm" id="tools">
+    <a className="btn btn-default" style={{margin: '7px'}} id="upload-button"><i className="glyphicon glyphicon-upload icon-white"></i> Upload</a>
+    <a className="btn btn-default" style={{margin: '7px'}} id="new-file-button"><i className="glyphicon glyphicon-file"></i> New File</a>
+    <a className="btn btn-default" style={{margin: '7px'}} id="new-folder-button"><i className="glyphicon glyphicon-folder-close"></i> New folder</a>
+    <a className="btn btn-default" style={{margin: '7px'}} id="refresh-button"><i className="glyphicon glyphicon-refresh"></i> Refresh</a>
+</div>
+
+
+<div className="container-table">
+
+    	 <table className="table table-list-search table table-hover">
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Created</th>
+                            <th>Modified</th>
+                            <th>Author</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Captain America</td>
+                            <td>capta</td>
+                            <td>89%</td>
+                            <td>02:14</td>
+
+                        </tr>
+                        <tr>
+                            <td>Spider-Man</td>
+                            <td>spida</td>
+                            <td>77%</td>
+                            <td>03:13</td>
+
+                        </tr>
+
+                        <tr>
+                            <td>Thor</td>
+                            <td>thor</td>
+                            <td>98%</td>
+                            <td>01:12</td>
+
+                        </tr>
+
+                    </tbody>
+        </table>
+	</div>
+
 
                         </div>
                       </div>
