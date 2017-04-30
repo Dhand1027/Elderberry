4d126d444aa09f3a40bbee1e967942e5f60a95e6
Antonio Borges <apborges@umass.edu>
2017-04-30 15:42:10 -0400

    fix boxing calendar

--- app/components/ui-calendar.js
+++ app/components/ui-calendar.js
@@ -8,20 +8,21 @@ require('style!css!react-big-calendar/lib/css/react-big-calendar.css')
 export default class UIcalendar extends React.Component {
       render () {
         return (
-          <div className="container-fluid">
-          <div className="row" >
-              <div className="col-md-10 col-xs-9 background">
-                  <div className="text-center">
-                      <div className="container">
-                          <div className="row row-height">
-                              <div className="col-md-12 boxing">
-                                  <div className="tabbable-panel-maps">
-                                      <div className="tabbable-line">
+          <div classNameName="container-fluid">
+              <div className="row" >
+                <div className="col-md-10 col-xs-9 background">
+                  <div className="text-center" >
+                    <div className="container">
+                      <div className="row row-height">
+                        <div className="col-md-12 boxing">
+                          <div className="tabbable-panel-maps">
+                            <div className="tabbable-line">
+
                                           <h2>Calendar</h2>
                                           <div className="tab-content">
                                               <div className= "row">
                                                 <div className= "col-md-12">
-                                                <BigCalendar style={{height: '300px'}} events={[]} />
+                                                <BigCalendar style={{height: '320px'}} events={[]} />
                                                 </div>
                                               </div>
                                             </div>
