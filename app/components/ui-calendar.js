9f3cde2c46b6e835541e533f882551d28a236406
Eric Kwan <ekwan@umass.edu>
2017-04-30 13:42:53 -0400

    getting calendar working

--- app/components/ui-calendar.js
+++ app/components/ui-calendar.js
@@ -1,34 +1,39 @@
 import React from 'react';
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
 
+BigCalendar.momentLocalizer(moment)
+require('style!css!react-big-calendar/lib/css/react-big-calendar.css')
 
 export default class UIcalendar extends React.Component {
-  render() {
-    return (
-
-      <div classNameName="container-fluid">
+      render () {
+        return (
+          <div className="container-fluid">
           <div className="row" >
-            <div className="col-md-10 col-xs-9 background">
-              <div className="text-center" >
-                <div className="container">
-                  <div className="row row-height">
-                    <div className="col-md-12 boxing">
-                      <div className="tabbable-panel-maps">
-                        <div className="tabbable-line">
-
-                          <h2>Calendar</h2>
-
-                               (Calendar here)
-
-
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-      </div>
+              <div className="col-md-10 col-xs-9 background">
+                  <div className="text-center">
+                      <div className="container">
+                          <div className="row row-height">
+                              <div className="col-md-12 boxing">
+                                  <div className="tabbable-panel">
+                                      <div className="tabbable-line">
+                                          <h2>Calendar</h2>
+                                          <div className="tab-content">
+                                              <div className= "row">
+                                                <div className= "col-md-12">
+                                                <BigCalendar style={{height: '420px'}} events={[]} />
+                                                </div>
+                                              </div>
+                                            </div>
+                                          </div>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
     )
   }
 }
